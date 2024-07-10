858864e947641dcb946084ea1deef23fc69c1389
Ben Purinton <ben.purinton@gmail.com>
2024-07-10 09:26:10 -0700

    fix spec hint for to_fs

--- spec/features/params_spec.rb
+++ spec/features/params_spec.rb
@@ -364,7 +364,7 @@ describe "/payment/new" do
 end
 
 describe "/payment/new" do
-  it "formats large dollar amounts on the results page", points: 1, hint: h("to_s_currency") do
+  it "formats large dollar amounts on the results page", points: 1, hint: h("to_fs_currency") do
     visit "/payment/new"
 
     # fill_in "APR", with: 4.10
@@ -447,7 +447,7 @@ describe "/payment/new" do
 end
 
 describe "/payment/new" do
-  it "calculates the monthly payment correctly", points: 1, hint: h("to_s_currency") do
+  it "calculates the monthly payment correctly", points: 1, hint: h("to_fs_currency") do
     visit "/payment/new"
 
     # fill_in "APR", with: 4.10
