888f810928432ea4d6fca3dc1650473c043d6710
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-18 00:33:58 +0000

    first_try

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+
+
+get("/howdy") do
+  erb(:hello)
 end
