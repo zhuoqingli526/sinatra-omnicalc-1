9af46261ad923f9aeddd20fd4faa115797cc9d0c
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-19 06:45:42 +0000

    square_root

--- app.rb
+++ app.rb
@@ -1,17 +1,41 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  erb(:homepage)
-end
 
 get("/square/new") do
   erb(:square_form)
 end
 
 get("/square/results") do
-  @number = params.fetch("number").to_i
-  @result = @number ** 2
+  @number = params.fetch("number")
+  @result = @number.to_f ** 2
 
   erb(:square_result)
+end
+
+get("/square_root/new") do
+  erb(:square_root_form)
+end
+
+get("/square_root/results") do
+  @number = params.fetch("number")
+  @result = Math.sqrt(@number.to_f)
+  erb(:square_root_result)
+end
+
+get("/square/results") do
+  @number = params.fetch("number")
+  @result = @number.to_f ** 2
+
+  erb(:square_result)
+end
+
+get("/random/new") do
+  erb(:square_root_form)
+end
+
+get("/random/results") do
+  @number = params.fetch("number")
+  @result = Math.sqrt(@number.to_f)
+  erb(:square_root_result)
 end
