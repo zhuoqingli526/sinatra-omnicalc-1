42a2ee9df6626bce18f8757e78e50c76d955eca1
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-19 07:51:28 +0000

    payment

--- app.rb
+++ app.rb
@@ -45,9 +45,15 @@ get("/payment/new") do
   erb(:payment_form)
 end
 
-get("/random/results") do
-  @min = params.fetch("user_min").to_f
-  @max = params.fetch("user_max").to_f
-  @random_result = rand(@min..@max)
-  erb(:random_result)
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @r = @apr/100/12
+  @year = params.fetch("user_years").to_i
+  @n = @year/12
+  @pv = params.fetch("user_pv").to_f
+  @numerator = @r * @pv
+  @denumerator = 1 - (1+@r) ** -@n
+
+  @payment_result = @numerator / @denumerator
+  erb(:paymemt_result)
 end
